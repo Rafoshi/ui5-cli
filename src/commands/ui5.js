9922a0376eb066038f3e5e66550a760d2a59021e
Daniel <srdaniiel@gmail.com>
2019-07-04 14:36:08 -0300

    gia de comandos

--- src/commands/ui5.js
+++ src/commands/ui5.js
@@ -3,6 +3,16 @@ module.exports = {
   run: async toolbox => {
     const { print } = toolbox
 
-    print.info('Welcome to your CLI')
+    print.info('******* Bem vindo ao UI5 CLI *******')
+    print.info('Criar página:')
+    print.success('ui5 g page companyList')
+    print.info('\nCriar página com parametros:')
+    print.success('ui5 g page companyEdit docEntry')
+
+    print.info('\nCriar fragmento:')
+    print.success('ui5 g f companyEditFields')        
+    
+    print.info('\nCriar rota:')
+    print.success('ui5 g r companyAnotherRoute')    
   }
 }
