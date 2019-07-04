09122142e873c086278f8f4e69e2679ea55165b2
Daniel <srdaniiel@gmail.com>
2019-07-04 14:26:57 -0300

    separação nos templates, melhoria na criação

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -4,96 +4,58 @@ module.exports = {
   run: async toolbox => {
     const {
       parameters,
-      template: { generate },
-      print: { info, error, success },
-      filesystem
+      createView,
+      createLabel,
+      createMenu,
+      createRoute,
+      createController,
+      createFragment
     } = toolbox
   
     
     if(!parameters.first){
-      error("É preciso especificar o tipo [ [p]age | [f]ragment]");
+      error("É preciso especificar o tipo [ [p]age | [f]ragment | [r]oute ]");
       return;
-    }    
+    } 
+
     if(!parameters.second){
       error("É preciso especificar o nome");
       return;
     }    
-    const formatCaptalize = (sName) => {
-      if(!sName) return;
-      return sName[0].toUpperCase() + sName.slice(1);
-    }
+    
     const type = parameters.first.toLowerCase();
-    const folder = parameters.second;
-    const name = formatCaptalize(folder);
-    const destination = `webapp/src/pages/${folder}/${name}`;
-    
-    const packagePath = 'webapp/manifest.json';
-    const appMenuPath = 'webapp/model/data/AppModel.json';
-    const i18nPath = 'webapp/i18n/i18n.properties';
-    
-    const package = await filesystem.read(packagePath, 'json');
-    const appMenu = await filesystem.read(appMenuPath, 'json');
-    let i18n = await filesystem.read(i18nPath);
-    
-    const namespace = package.name;
-    const destinationTarget = `${namespace}.src.pages.${folder}`;
+    const folder = parameters.second;   
     const isPAge = (type[0] == 'p')
-
-    let route = {
-      pattern: folder,
-      name: folder,
-      target: folder
-    };
-
-    let target = 
-      {
-        viewName: name,
-        viewLevel: 3,
-        viewPath: destinationTarget,        
-      }
+    const isRoute = (type[0] == 'r')
+    const isFragment = (type[0] == 'f')
     
-    let menu =  {
-      title: `Commom.${name}`,
-      "icon": "sap-icon://error",
-      "expanded": true,
-      "key": folder
-    }
-    i18n += `\r\nCommom.${name}=${name}`
-
-    appMenu.navigation = appMenu.navigation.filter(n => n.key != folder);
-    appMenu.navigation.push(menu)
-    
-    package['sap.ui5'].routing.routes = package['sap.ui5'].routing.routes.filter(x => x.pattern != route.pattern);
-    package['sap.ui5'].routing.routes.push(route);
-    package['sap.ui5'].routing.targets[route.pattern] = target;
-    
-    if(isPAge){
-      await generate({
-        template: 'view.js.ejs',
-        target: `${destination}.view.xml`,
-        props: { name, folder, namespace }
-      });
+    if(isPAge){           
+      parameters.array.shift()
+      parameters.array.shift();
       
-      await generate({
-        template: 'controller.js.ejs',
-        target: `${destination}.controller.js`,
-        props: { name, folder, namespace }
-      })
-      filesystem.write(packagePath, package)
-      filesystem.write(appMenuPath, appMenu)
-      filesystem.write(i18nPath, i18n)
-
-      success(`arquivos gerados em: ${destination}`);
+      createView(folder);
+      createController(folder);
+      createLabel(folder);       
+      createRoute(folder, parameters.array)
+      if(parameters.array.length == 0) {
+        createMenu(folder);
+      }
     }
-    else{
-      await generate({
-        template: 'fragment.js.ejs',
-        target: `${destination}.fragment.xml`,
-        props: { name, folder, namespace }
-      });
 
-      success(`arquivos gerados em: ${destination}`);
+    if(isRoute){
+      parameters.array.shift()
+      parameters.array.shift();
+      
+      createRoute(folder, parameters.array)
+      if(parameters.array.length == 0) {
+        createMenu(folder);
+      }
     }
+
+    if(isFragment){
+      createFragment(folder)
+    }
+    
 
    
    
--- src/extensions/cli-extension.js
+++ src/extensions/cli-extension.js
@@ -1,15 +1,189 @@
 // add your CLI-specific functionality here, which will then be accessible
 // to your commands
 module.exports = toolbox => {
-  toolbox.foo = () => {
-    toolbox.print.info('called foo extension')
+
+  const {
+    template: {
+      generate
+    },
+    print: {
+      info,
+      error,
+      success
+    },
+    filesystem
+  } = toolbox
+
+  async function getManifest() {
+    const manifestPath = 'webapp/manifest.json';
+    const manifest = await filesystem.read(manifestPath, 'json');
+    return manifest;
+  }
+  async function saveManifest(manifest) {
+    const manifestPath = 'webapp/manifest.json';
+    filesystem.write(manifestPath, manifest)
+    success(`Manifest Alterado: ${manifestPath}`)
+   
   }
 
-  // enable this if you want to read configuration in from
-  // the current folder's package.json (in a "ui5" property),
-  // ui5.config.json, etc.
-  // toolbox.config = {
-  //   ...toolbox.config,
-  //   ...toolbox.config.loadConfig(process.cwd(), "ui5")
-  // }
+  async function getNamespace() {
+    const namespace = await getManifest().name;
+    return namespace;
+
+  }
+  const formatCaptalize = (sName) => {
+    if (!sName) return;
+    return sName[0].toUpperCase() + sName.slice(1);
+  }
+  async function addRoute(route, target) {
+    let manifest = await getManifest();
+    const manifestPath = 'webapp/manifest.json';
+
+    manifest['sap.ui5'].routing.routes = manifest['sap.ui5'].routing.routes.filter(x => x.pattern != route.pattern);
+    manifest['sap.ui5'].routing.routes.push(route);
+    manifest['sap.ui5'].routing.targets[route.target] = target;
+    filesystem.write(manifestPath, manifest)
+  }
+
+  async function getProperties(folder, params) {
+    let manifest = await getManifest();
+    let namespace = manifest.name;
+    let pattern = `${folder}`;
+    let target = `${folder}`;
+    let name = formatCaptalize(folder)
+    let segments = ''
+    let destTarget = `${namespace}.src.pages.${folder}`;
+    let destination = `webapp/src/pages/${folder}/${name}`;
+
+    if (params) segments = params.map(z => `{${z}}`).join('/');
+    if (segments != '') pattern += `/${segments}`;
+
+    return {
+      namespace,
+      name,
+      segments,
+      destTarget,
+      pattern,
+      destination,
+      target
+    };
+  }
+
+  async function createRoute(folder, params) {
+
+    let props = await getProperties(folder, params);
+
+    let route = {
+      pattern: props.pattern,
+      name: folder,
+      target: props.target
+    };
+
+    let target = {
+      viewName: props.target,
+      viewLevel: 3,
+      viewPath: props.destTarget,
+    }
+
+    manifest = addRoute(route, target)
+    saveManifest(manifest);
+    info(`--Adicionado nova rota: ${props.pattern}`)
+    info(`--Adicionado target para rota: ${props.target}`)
+  }
+  async function createLabel(folder) {
+    let name = formatCaptalize(folder);
+    const i18nPath = 'webapp/i18n/i18n.properties';
+    let i18n = await filesystem.read(i18nPath);
+    let text= `Commom.${name}=${name}`;
+    i18n+=`\r\n${text}`;
+
+    filesystem.write(i18nPath, i18n)
+    success(`i18n Alterado ${i18nPath}`)
+    info(`--Adicionado: ${text}`)
+  }
+
+  async function createMenu(folder) {
+    const appMenuPath = 'webapp/model/data/AppModel.json';
+    const appMenu = await filesystem.read(appMenuPath, 'json');
+    let name = formatCaptalize(folder);
+    let title =`Commom.${name}`
+    let menu = {
+      title,
+      "icon": "sap-icon://error",
+      "expanded": true,
+      "key": folder
+    }
+
+    appMenu.navigation = appMenu.navigation.filter(n => n.key != folder);
+    appMenu.navigation.push(menu);
+
+    filesystem.write(appMenuPath, appMenu)
+    success(`JSON MENU alterado: ${appMenuPath}`)
+    info(`--Adicionado menu: key=${folder} title=${title}`)
+
+  }
+
+  async function removeMenu(folder) {
+    const appMenuPath = 'webapp/model/data/AppModel.json';
+    const appMenu = await filesystem.read(appMenuPath, 'json');
+    appMenu.navigation = appMenu.navigation.filter(n => n.key != folder);
+    filesystem.write(appMenuPath, appMenu)
+  }
+  async function createView(folder, segments) {
+    let props = await getProperties(folder, segments);
+    let target = `${props.destination}.view.xml`;
+
+    await generate({
+      template: 'view.js.ejs',
+      target,
+      props: {
+        name: props.name,
+        folder: folder,
+        namespace: props.namespace
+      }
+    });
+
+    success(`VIEW CRIADA: ${target}`)
+  }
+
+  async function createController(folder) {
+    let props = await getProperties(folder);
+    let controller = `${props.destination}.controller.js`;
+    await generate({
+      template: 'controller.js.ejs',
+      target: controller,
+      props: {
+        name: props.name,
+        folder: folder,
+        namespace: props.namespace
+      }
+    })
+    success(`CONTROLLER CRIADO: ${controller}`);
+
+  }
+
+  async function createFragment(folder) {
+
+    let props = await getProperties(folder);
+    let target = `${props.destination}.fragment.xml`;
+    await generate({
+      template: 'fragment.js.ejs',
+      target,
+      props: {
+        name: props.name,
+        folder,
+        namespace: props.namespace
+      }
+    });
+
+    success(`FRAGMENTO CRIADO: ${target}`);
+  }
+
+  toolbox.createController = createController;
+  toolbox.createMenu = createMenu
+  toolbox.createRoute = createRoute
+  toolbox.createView = createView
+  toolbox.createLabel = createLabel
+  toolbox.createFragment = createFragment
+
 }
